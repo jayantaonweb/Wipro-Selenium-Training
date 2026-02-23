b7bf7a62483f93a401b7531c81d976e78774aeab
Jayanta <146463712+jayantaonweb@users.noreply.github.com>
2026-02-23 10:09:01 +0530

    Add files via upload

---
+++ Selenium_Pytest_PMO/conftest.py
@@ -0,0 +1,21 @@
+# create a fixture for invoke chrome browser
+
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope = "class")
+def setup(request):
+    service = Service(ChromeDriverManager().install())
+    #driver instance is created to use globally in test script
+    driver = webdriver.Chrome(service = service)
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+
+    request.cls.driver = driver
+    yield
+    driver.quit()
