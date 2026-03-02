d4539f11375c7f4633740c6f1dab06b87b7cbbe6
Jayanta <146463712+jayantaonweb@users.noreply.github.com>
2026-03-02 10:00:12 +0530

    Add files via upload

--- Selenium_Pytest_PMO/conftest.py
+++ Selenium_Pytest_PMO/conftest.py
@@ -1,5 +1,5 @@
-# create a fixture for invoke chrome browser
-
+import os
+from datetime import datetime
 
 import pytest
 from selenium import webdriver
@@ -7,15 +7,48 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture(scope = "function")
-def driver(request):
-    service = Service(ChromeDriverManager().install())
-    #driver instance is created to use globally in test script
-    driver = webdriver.Chrome(service = service)
-    driver.maximize_window()
-    driver.implicitly_wait(15)
-    driver.get("https://opensource-demo.orangehrmlive.com/")
+@pytest.fixture(scope="function")
+def driver():
+    options = webdriver.ChromeOptions()
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "profile.password_manager_leak_detection": False
+    }
 
-    request.cls.driver = driver
-    yield
+    options.add_experimental_option("prefs", prefs)
+    options.add_argument("--incognito")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-features=PasswordLeakDetection")
+    options.add_argument("--disable-features=PasswordManagerOnboarding")
+    options.add_argument("--disable-features=AutofillServerCommunication")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+
+    yield driver
     driver.quit()
+
+# screenshot hook if testcase fails
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver", None)
+        if driver:
+            screenshots_dir = "screenshots"
+            os.makedirs(screenshots_dir, exist_ok=True)
+
+            file_name = f"{item.name}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
+            file_path = os.path.join(screenshots_dir, file_name)
+
+            driver.save_screenshot(file_path)
+
+
